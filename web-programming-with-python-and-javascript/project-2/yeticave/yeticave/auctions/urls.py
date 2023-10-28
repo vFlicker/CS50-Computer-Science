5983df0010a169db1ab0e647127246668a88a0bb
Vladislav <flickervladislav@gmail.com>
2023-10-28 21:43:20 +0300

    refactor: change file structure

---
+++ web-programming-with-python-and-javascript/project-2/yeticave/yeticave/auctions/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("listing/<int:listing_id>/", views.ListingView.as_view(), name="listing"),
+    path("listing/<int:listing_id>/toggle_watchlist/", views.toggle_watchlist, name="toggle_watchlist"),
+    path("listing/<int:listing_id>/close_bid/", views.close_bid, name="close_bid"),
+    path("create_listing/", views.create_listing, name="create_listing"),
+    path("watchlist/", views.watchlist, name="watchlist"),
+    path("categories/<int:category_id>/", views.categories, name="categories"),
+]
