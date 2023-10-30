e062d906af70435e81234369cf102432b41170ec
Vladislav <flickervladislav@gmail.com>
2023-10-30 12:57:57 +0200

    fix: routing

--- web-programming-with-python-and-javascript/project-2/yeticave/yeticave/accounts/urls.py
+++ web-programming-with-python-and-javascript/project-2/yeticave/yeticave/accounts/urls.py
@@ -7,6 +7,6 @@ from . import views
 app_name = "accounts"
 urlpatterns = [
     path("register/", views.register, name="register"),
-    path("login/", LoginView.as_view(), name="login"),
+    path("login/", LoginView.as_view(next_page="/"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
